15593222cc4dfb44c58706ac72eac2c4061658cb
Ashish Jain <ashishjain@gamestop.com>
2019-09-05 21:00:18 -0500

    commit

--- VaccumRoom/Program.cs
+++ VaccumRoom/Program.cs
@@ -7,31 +7,14 @@ namespace VacuumRoom
         static void Main(string[] args)
         {
 
-            IVacuumRobot vacuumRobot = new VacuumRobot();
-
             //VacuumRobot() using new vacuum robot
             Console.WriteLine("Starting New Vacuum Implementation.....");
-            VacuumRobotMovements(vacuumRobot);
+            VacuumRoom.VacuumRobotMovements(new VacuumRobot());
 
             //VacuumRobot() using Legacy Vacuum Robot
             Console.WriteLine("Starting Legacy Vacuum Implementation.....");
-            vacuumRobot = new LegacyVacuumAdapter(new LegacyVacuumRobot());
-            VacuumRobotMovements(vacuumRobot);
-
+            VacuumRoom.VacuumRobotMovements(new LegacyVacuumAdapter(new LegacyVacuumRobot()));
             Console.ReadLine();
-        }
-
-        private static void VacuumRobotMovements(IVacuumRobot vacuumRobot)
-        {
-            Console.WriteLine("-----------------------------------------------------");
-            vacuumRobot.MoveForward(10);
-            Console.WriteLine("-----------------------------------------------------");
-            vacuumRobot.MoveBackward(5);
-            Console.WriteLine("-----------------------------------------------------");
-            vacuumRobot.RotateRight(180);
-            Console.WriteLine("-----------------------------------------------------");
-            vacuumRobot.RotateLeft(90);
-            Console.WriteLine("-----------------------------------------------------");
         }
     }
 }
---
+++ VaccumRoom/VacuumRoom.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VacuumRoom
+{
+    public class VacuumRoom
+    {
+        public static void VacuumRobotMovements(IVacuumRobot vacuumRobot)
+        {
+            Console.WriteLine("-----------------------------------------------------");
+            vacuumRobot.MoveForward(10);
+            Console.WriteLine("-----------------------------------------------------");
+            vacuumRobot.MoveBackward(5);
+            Console.WriteLine("-----------------------------------------------------");
+            vacuumRobot.RotateRight(180);
+            Console.WriteLine("-----------------------------------------------------");
+            vacuumRobot.RotateLeft(90);
+            Console.WriteLine("-----------------------------------------------------");
+        }
+    }
+}
